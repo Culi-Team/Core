a1070883b89e85e9fb22e582484b7f70e26a585f
DungNH <nguyenhuudung.1310@gmail.com>
2024-01-06 14:28:47 +0700

    Add IMotion

---
+++ EQX.Core/Motions/IMotion.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EQX.Core.Motions
+{
+    public interface IMotion
+    {
+        int Id { get; }
+        string Name { get; }
+
+        bool Connect();
+        bool Disconnect();
+
+        bool MotionOn();
+        bool MotionOff();
+
+        bool SearchOrigin();
+
+        bool MoveInc(double position, double speed);
+        bool MoveAbs(double position, double speed);
+
+        bool Stop(bool forceStop = false);
+    }
+}

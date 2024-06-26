0c2d98f074e29b1956eeb3f9ae5680b82220625b
휴중 (NGUYEN HUU DUNG) <huudung@topengnet.com>
2024-06-26 18:03:10 +0700

    Add Robot device

---
+++ EQX.Core/Robot/IRobot.cs
@@ -0,0 +1,15 @@
+﻿using EQX.Core.Common;
+
+namespace EQX.Core.Robot
+{
+    public delegate void RobotResponseHandler(object sender, string response);
+
+    public interface IRobot : IIdentifier, IHandleConnection
+    {
+        bool SendCommand(string command);
+        string ReadResponse(int timeoutMs);
+        Task<string> ReadResponseAsync(int timeoutMs);
+
+        RobotResponseHandler? OnRobotResponsed { get; }
+    }
+}

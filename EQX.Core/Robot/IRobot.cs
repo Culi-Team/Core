c4ad1a4b79eaebae9374d5c9da9ed74ed7a6ed7b
휴중 (NGUYEN HUU DUNG) <huudung@topengnet.com>
2024-07-01 19:48:29 +0700

    Implement KukaRobot Interface

--- EQX.Core/Robot/IRobot.cs
+++ EQX.Core/Robot/IRobot.cs
@@ -6,8 +6,23 @@ namespace EQX.Core.Robot
 
     public interface IRobot : IIdentifier, IHandleConnection
     {
+        /// <summary>
+        /// Send command from PC to Robot
+        /// </summary>
+        /// <param name="command">Command</param>
+        /// <returns>true if success; false if fail</returns>
         bool SendCommand(string command);
+        /// <summary>
+        /// Read response from Robot
+        /// </summary>
+        /// <param name="timeoutMs">Time out in millisecond</param>
+        /// <returns></returns>
         string ReadResponse(int timeoutMs);
+        /// <summary>
+        /// Read response from Robot asynchronous
+        /// </summary>
+        /// <param name="timeoutMs">Time out in millisecond</param>
+        /// <returns></returns>
         Task<string> ReadResponseAsync(int timeoutMs);
 
         RobotResponseHandler? OnRobotResponsed { get; }

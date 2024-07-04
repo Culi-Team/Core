dbdbe185bcddc8130f58ccf3c52c1b135f8e54b1
휴중 (NGUYEN HUU DUNG) <huudung@topengnet.com>
2024-07-04 19:59:16 +0700

    RootProcess implement

---
+++ EQX.Core/Common/MultiThreadingHelpers.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EQX.Core.Common
+{
+    public static class MultiThreadingHelpers
+    {
+        public static void SafeSetValue<T>(ref T target, T value) where T : class
+        {
+            Interlocked.Exchange<T>(ref target, value);
+        }
+
+        public static void SafeSetValue<T>(ref int target, T value) where T : Enum
+        {
+            Interlocked.Exchange(ref target, Convert.ToInt32(value));
+        }
+
+        public static void SafeSetValue(ref int target, int value)
+        {
+            Interlocked.Exchange(ref target, value);
+        }
+    }
+}

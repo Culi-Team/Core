b6fafc750dba9ebddb4350df8e8c7f414286bede
휴중 (NGUYEN HUU DUNG) <huudung@topengnet.com>
2024-07-12 20:06:11 +0700

    Add Cylinder

---
+++ EQX.Core/InOut/Cylinder/ICylinder.cs
@@ -0,0 +1,17 @@
+﻿using EQX.Core.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EQX.Core.InOut
+{
+    public interface ICylinder
+    {
+        bool IsForward { get; }
+        bool IsBackward { get; }
+        void MoveForward();
+        void MoveBackward();
+    }
+}

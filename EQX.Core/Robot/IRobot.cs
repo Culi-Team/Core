eb25ffa972d281ae98ece894e955f2b6d2cc9fce
휴중 (NGUYEN HUU DUNG) <huudung@topengnet.com>
2024-07-16 10:27:15 +0700

    Add SimulationRobot

--- EQX.Core/Robot/IRobot.cs
+++ EQX.Core/Robot/IRobot.cs
@@ -18,6 +18,7 @@ namespace EQX.Core.Robot
         /// <param name="timeoutMs">Time out in millisecond</param>
         /// <returns></returns>
         string ReadResponse(int timeoutMs);
+        bool ReadResponse(int timeoutMs, string expectedResponse);
         /// <summary>
         /// Read response from Robot asynchronous
         /// </summary>

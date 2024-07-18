7214c308370dd58ad1f3212be6fa047f292c20c1
휴중 (NGUYEN HUU DUNG) <huudung@topengnet.com>
2024-07-18 15:49:45 +0700

    TrayCell Status binding implementation

--- EQX.Core/Units/ITrayCell.cs
+++ EQX.Core/Units/ITrayCell.cs
@@ -11,15 +11,4 @@ namespace EQX.Core.Units
     {
         TECellStatus Status { get; set; }
     }
-
-    public class TrayCell<TECellStatus> : ITrayCell<TECellStatus> where TECellStatus : Enum
-    {
-        public TECellStatus Status { get; set; }
-        public int Id { get; }
-
-        public TrayCell(int id)
-        {
-            Id = id;
-        }
-    }
 }
---
+++ EQX.Core/Units/TrayCell.cs
@@ -0,0 +1,13 @@
+﻿namespace EQX.Core.Units
+{
+    public class TrayCell<TECellStatus> : ITrayCell<TECellStatus> where TECellStatus : Enum
+    {
+        public TECellStatus Status { get; set; }
+        public int Id { get; }
+
+        public TrayCell(int id)
+        {
+            Id = id;
+        }
+    }
+}

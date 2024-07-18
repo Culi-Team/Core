a9144b8b8b27675b5b4cb3f88482da2f2a544089
Nguyễn Huy Ngọ <huyngu20@gmail.com>
2024-07-18 23:57:28 +0700

    update lastest submodule

--- EQX.Core/Common/AlarmModel.cs
+++ EQX.Core/Common/AlarmModel.cs
@@ -5,7 +5,22 @@
         public int Id { get; set; }
         public string Message { get; set; }
         public string AlarmOverviewSource { get; set; }
+        public InfoRectangleDrawOnAlarm InfoRectangleDrawOnAlarmOverview { get; set; }
         public string AlarmDetailviewSource { get; set; }
+        public InfoRectangleDrawOnAlarm InfoRectangleDrawOnAlarmDetailview { get; set; }
         public List<string> TroubleshootingSteps { get; set; }
+        public string TroubleshootingStepToString
+        {
+            get
+            {
+                string ans = string.Empty;
+                for (int i = 0; i < TroubleshootingSteps.Count - 1; ++i)
+                {
+                    ans += TroubleshootingSteps[i] + "=>";
+                }
+                ans += TroubleshootingSteps[TroubleshootingSteps.Count - 1];
+                return ans;
+            }
+        }
     }
 }

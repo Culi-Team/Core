45749a071985a299f1d99293899b8564ed9ce866
Xuanhv21 <xuanhoangvinh01@gmail.com>
2024-07-18 15:26:14 +0700

    Add DescriptionAttribute/ Unit

---
+++ EQX.Core/Recipe/SingleRecipeDescriptionAttribute.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EQX.Core.Recipe
+{
+    [AttributeUsage(AttributeTargets.Property)]
+    public class SingleRecipeDescriptionAttribute : Attribute
+    {
+        public int Index { get; set; }
+        public string Description { get; set; }
+        public string Unit { get; set; }
+        public bool IsSpacer { get; set; } = false;
+    }
+}

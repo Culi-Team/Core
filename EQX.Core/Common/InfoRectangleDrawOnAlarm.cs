86f5f34a7ece72997cd30f1c1cda06a20b2124d6
Nguyễn Huy Ngọ <huyngu20@gmail.com>
2024-07-18 16:39:02 +0700

    Add InfoRectangleDrawOnAlarm

---
+++ EQX.Core/Common/InfoRectangleDrawOnAlarm.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EQX.Core.Common
+{
+    public class InfoRectangleDrawOnAlarm
+    {
+        public double CVLeft { get; set; }
+        public double CVTop { get; set; }
+        public double Width { get; set; }
+        public double Height { get; set; }
+    }
+}

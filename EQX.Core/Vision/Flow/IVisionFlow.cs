b497f87e460ed58d8b8087f6c7236ffe56b5dabd
Xuanhv21 <xuanhoangvinh01@gmail.com>
2024-07-19 19:05:16 +0700

    Add delegate VisionFlowRunFinished

--- EQX.Core/Vision/Flow/IVisionFlow.cs
+++ EQX.Core/Vision/Flow/IVisionFlow.cs
@@ -4,11 +4,13 @@ using System.Collections.ObjectModel;
 
 namespace EQX.Core.Vision.Flow
 {
+    public delegate void VisionFlowRunFinishedHandler(object sender, IObjectCollection result);
     public interface IVisionFlow : IIdentifier, IAsyncRunable
     {
         ObservableCollection<IVisionTool> VisionTools { get; }
         ObservableCollection<VisionToolConnection> VisionToolConnections { get; }
         IObjectCollection Outputs { get; }
+        event VisionFlowRunFinishedHandler VisionFlowRunFinished;
         void Save();
     }
 }

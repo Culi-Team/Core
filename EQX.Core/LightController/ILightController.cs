57e0670183e808d3e998250833439f795f450c96
Xuanhv21 <xuanhoangvinh01@gmail.com>
2024-07-19 09:21:06 +0700

    Add LightController

---
+++ EQX.Core/LightController/ILightController.cs
@@ -0,0 +1,21 @@
+﻿using EQX.Core.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EQX.Core.LightController
+{
+    public interface ILightController : IIdentifier, ILogable
+    {
+        int NumberOfChannel { get; set; }
+        bool Open();
+        void Close();
+        void SetLightLevel(int channel, int value);
+        void SetLightStatus(int channel, bool bOnOff);
+
+        int GetLightLevel(int channel);
+        bool GetLightStatus(int channel);
+    }
+}

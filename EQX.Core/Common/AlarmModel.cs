63e0ca055b0d97aec1abc6b56b6470b248b2c14b
휴중 (NGUYEN HUU DUNG) <nguyenhuudung.1310@gmail.com>
2024-07-22 10:56:44 +0700

    Update AlarmNofityView

--- EQX.Core/Common/AlarmModel.cs
+++ EQX.Core/Common/AlarmModel.cs
@@ -1,26 +1,18 @@
-﻿namespace EQX.Core.Common
+﻿using System.Drawing;
+
+namespace EQX.Core.Common
 {
     public class AlarmModel
     {
         public int Id { get; set; }
         public string Message { get; set; }
+
         public string AlarmOverviewSource { get; set; }
-        public InfoRectangleDrawOnAlarm InfoRectangleDrawOnAlarmOverview { get; set; }
+        public Rectangle AlarmOverviewHighlightRectangle { get; set; }
+
         public string AlarmDetailviewSource { get; set; }
-        public InfoRectangleDrawOnAlarm InfoRectangleDrawOnAlarmDetailview { get; set; }
+        public Rectangle AlarmDetailviewHighlightRectangle { get; set; }
+
         public List<string> TroubleshootingSteps { get; set; }
-        public string TroubleshootingStepToString
-        {
-            get
-            {
-                string ans = string.Empty;
-                for (int i = 0; i < TroubleshootingSteps.Count - 1; ++i)
-                {
-                    ans += TroubleshootingSteps[i] + "=>";
-                }
-                ans += TroubleshootingSteps[TroubleshootingSteps.Count - 1];
-                return ans;
-            }
-        }
     }
 }

6c9a36a55a9f711cf751867c2a5600d9b0effe10
Xuanhv21 <xuanhoangvinh01@gmail.com>
2024-07-24 13:31:56 +0700

    Add VisionResult

---
+++ EQX.Core/Vision/IVisionResult.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EQX.Core.Vision
+{
+    public enum EVisionJudge
+    {
+        NG = -1,
+        OK = 1
+    }
+    public interface IVisionResult
+    {
+        EVisionJudge Judge { get; }
+    }
+}

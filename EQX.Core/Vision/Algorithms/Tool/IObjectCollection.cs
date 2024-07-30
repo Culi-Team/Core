e897833da8f471791b42d543e569007b8266a848
NGUYỄN HỮU DŨNG <nguyenhuudung.1310@gmail.com>
2024-07-30 21:50:07 +0700

    Passing Type to Key of ObjectCollection

--- EQX.Core/Vision/Algorithms/Tool/IObjectCollection.cs
+++ EQX.Core/Vision/Algorithms/Tool/IObjectCollection.cs
@@ -1,10 +1,33 @@
-﻿using System.Collections.ObjectModel;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System.Collections.ObjectModel;
 
 namespace EQX.Core.Vision.Algorithms
 {
+    public class KeyType : ObservableObject
+    {
+        private string key;
+
+        public string Key 
+        {
+            get => key;
+            set
+            {
+                key = value;
+                OnPropertyChanged(nameof(Key));
+            }
+        }
+        public Type Type { get; set; }
+
+        public KeyType(string key, Type type)
+        {
+            Key = key;
+            Type = type;
+        }
+    }
+
     public interface IObjectCollection
     {
         object? this[string key] { get; set; }
-        ObservableCollection<string>? Keys { get; set; }
+        ObservableCollection<KeyType>? Keys { get; set; }
     }
 }

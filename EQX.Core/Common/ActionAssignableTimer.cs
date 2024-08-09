2517f141eba6467ae727009965f595badcb840a8
NGUYỄN HỮU DŨNG <nguyenhuudung.1310@gmail.com>
2024-08-09 09:00:05 +0700

    Add ActionAssignableTimer

--- EQX.Core/Common/ActionAssignableTimer.cs
+++ EQX.Core/Common/ActionAssignableTimer.cs
@@ -4,6 +4,12 @@
     {
         public string Name { get; }
 
+        public ActionAssignableTimer()
+        {
+            phase1Actions = new Dictionary<string, Action>();
+            phase2Actions = new Dictionary<string, Action>();
+        }
+
         public void StartTimer(int intervalMs)
         {
             if (timer != null && timer.Enabled)
@@ -16,20 +22,38 @@
             timer.Enabled = true;
         }
 
+        public void EnableAction(string identityKey, Action phase1Action, Action phase2Action)
+        {
+            if (phase1Actions.ContainsKey(identityKey) == false && phase1Action != null)
+            {
+                phase1Actions.Add(identityKey, phase1Action);
+            }
+            if (phase2Actions.ContainsKey(identityKey) == false && phase2Action != null)
+            {
+                phase2Actions.Add(identityKey, phase2Action);
+            }
+        }
+
+        public void DisableAction(string identityKey)
+        {
+            phase1Actions.Remove(identityKey);
+            phase2Actions.Remove(identityKey);
+        }
+
         private void Timer_Elapsed(object? sender, System.Timers.ElapsedEventArgs e)
         {
             count++;
 
             if (phaseNumber == 1)
             {
-                foreach (var action in phase1Action)
+                foreach (var action in phase1Actions)
                 {
                     action.Value();
                 }
             }
             else
             {
-                foreach (var action in phase2Action)
+                foreach (var action in phase2Actions)
                 {
                     action.Value();
                 }
@@ -38,11 +62,15 @@
 
         public void StopTimer()
         {
+            phase1Actions.Clear();
+            phase2Actions.Clear();
 
+            timer.Stop();
+            timer.Dispose();
         }
 
-        private Dictionary<string, Action> phase1Action;
-        private Dictionary<string, Action> phase2Action;
+        private Dictionary<string, Action> phase1Actions;
+        private Dictionary<string, Action> phase2Actions;
         private System.Timers.Timer timer;
         private int count = 0;
         private int phaseNumber => count % 2;

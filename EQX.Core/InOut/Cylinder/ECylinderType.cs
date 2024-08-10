e0ceb8e633176d790cd3ddd2ad5905af4b63572a
NGUYỄN HỮU DŨNG <nguyenhuudung.1310@gmail.com>
2024-08-10 23:33:38 +0700

    Update CylinderView

--- EQX.Core/InOut/Cylinder/ECylinderType.cs
+++ EQX.Core/InOut/Cylinder/ECylinderType.cs
@@ -1,14 +1,27 @@
 ﻿namespace EQX.Core.InOut
 {
+    /// <summary>
+    /// <para/>Non Reverse :
+    /// <para/>    Forward = Up = Right = Grip = Algin = Lock
+    /// <para/>    Backward = Down = Left = Ungrip = Unalign = Unlock
+    /// 
+    /// <para/>Reverse :
+    /// <para/>    Forward = Down = Left = Ungrip = Unalign = Unlock
+    /// <para/>    Backward = Up = Right = Grip = Algin = Lock
+    /// </summary>
     public enum ECylinderType
     {
         ForwardBackward,
         ForwardBackwardReverse,
         UpDown,
         UpDownReverse,
-        LeftRight,
-        LeftRightReverse,
+        RightLeft,
+        RightLeftReverse,
         GripUngrip,
         GripUngripReverse,
+        AlignUnalign,
+        AlignUnalignReverse,
+        LockUnlock,
+        LockUnlockReverse,
     }
 }

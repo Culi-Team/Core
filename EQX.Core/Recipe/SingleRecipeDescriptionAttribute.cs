290a5ad0fc281bc2ce429382ea8b07c66f881b2d
hieu202 <hieubvc159@gmail.com>
2024-09-18 16:45:21 +0700

    Add  DescriptionKey DetailKey

--- EQX.Core/Recipe/SingleRecipeDescriptionAttribute.cs
+++ EQX.Core/Recipe/SingleRecipeDescriptionAttribute.cs
@@ -11,9 +11,11 @@ namespace EQX.Core.Recipe
     {
         public int Index { get; set; }
         public string Description { get; set; }
+        public string DescriptionKey { get; set; }
         public string Unit { get; set; }
         public bool IsSpacer { get; set; } = false;
         public string Detail { get; set; }
+        public string DetailKey { get; set; }
 
     }
 }

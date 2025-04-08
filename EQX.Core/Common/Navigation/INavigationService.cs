968b7d3dd4572fc5b495c2d5e56aa257db77f233
NGUYEN HUU DUNG <huudung@topengnet.com>
2025-04-08 11:10:32 +0700

    Update

---
+++ EQX.Core/Common/Navigation/INavigationService.cs
@@ -0,0 +1,9 @@
+﻿namespace EQX.Core.Common
+{
+    public interface INavigationService
+    {
+        event EventHandler? Navigating;
+
+        void NavigateTo<T>() where T : ViewModelBase;
+    }
+}
---
+++ EQX.Core/Common/Navigation/UserStore.cs
@@ -0,0 +1,19 @@
+﻿namespace EQX.Core.Common
+{
+    public class UserStore
+    {
+        public event Action UserChanged;
+        private EPermission permission = EPermission.Operator;
+
+        public EPermission Permission
+        {
+            get => permission;
+            set
+            {
+                permission = value;
+                UserChanged?.Invoke();
+            }
+        }
+    }
+
+}

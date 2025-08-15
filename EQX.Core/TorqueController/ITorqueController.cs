fd1dd2b4f455f1228045dd22973bdd4f75bc31ae
Xuanhv21 <xuanhoangvinh01@gmail.com>
2025-08-15 13:07:43 +0700

    Torque Control

---
+++ EQX.Core/TorqueController/ITorqueController.cs
@@ -0,0 +1,19 @@
+﻿using EQX.Core.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EQX.Core.TorqueController
+{
+    public interface ITorqueController : IIdentifier, IHandleConnection
+    {
+        void SetTorque(int index , int torque);
+        void Jog(int index, int speed, bool isForward);
+        void Stop(int index);
+        void SetSpeed(int index, int speed);
+        void ResetAlarm(int index);
+        void Run(int index);
+    }
+}

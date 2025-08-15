2d5b68f0aa18235cc86555d9bdc29a76569d4c25
Xuanhv21 <xuanhoangvinh01@gmail.com>
2025-08-15 14:26:28 +0700

    ITV Regulator Device

---
+++ EQX.Core/Device/Regulator/IRegulator.cs
@@ -0,0 +1,17 @@
+﻿using EQX.Core.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EQX.Core.Device.Regulator
+{
+    public interface IRegulator : IIdentifier, IHandleConnection
+    {
+        bool SetPressure(double value);
+        bool IncreasePressure();
+        bool DecreasePressure();
+        double GetPressure();
+    }
+}

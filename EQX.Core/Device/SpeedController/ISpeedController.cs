000096db5d2c0c7ad7f05756ee6d6deb2618085f
Xuanhv21 <xuanhoangvinh01@gmail.com>
2025-08-21 21:14:20 +0700

    Speed Controller

--- EQX.Core/Device/SpeedController/ISpeedController.cs
+++ EQX.Core/Device/SpeedController/ISpeedController.cs
@@ -5,9 +5,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace EQX.Core.Device.RollerController
+namespace EQX.Core.Device.SpeedController
 {
-    public interface IRollerController : IHandleConnection , IIdentifier
+    public interface ISpeedController : IHandleConnection , IIdentifier
     {
         void Start();
         void Stop();

3494d9052f7eb5387e1407fbe0aa9834f74a517d
Xuanhv21 <xuanhoangvinh01@gmail.com>
2025-08-21 18:10:37 +0700

    Add Roller Controller Device

---
+++ EQX.Core/Device/RollerController/IRollerController.cs
@@ -0,0 +1,19 @@
+﻿using EQX.Core.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EQX.Core.Device.RollerController
+{
+    public interface IRollerController : IHandleConnection , IIdentifier
+    {
+        void Start();
+        void Stop();
+        void SetDirection(bool isCW);
+        void SetSpeed(int speed);
+        void SetAcceleration(int acceleration);
+        void SetDeceleration(int deceleration);
+    }
+}

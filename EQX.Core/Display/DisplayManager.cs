2aa165d67cfd115331d6f270bb28bc68ea40d1a4
NGUYEN PHU TRONG <trongong123@gmail.com>
2025-09-11 13:59:48 +0700

    Add 2 Screen View Option

---
+++ EQX.Core/Display/DisplayManager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EQX.Core.Display
+{
+    public class DisplayManager
+    {
+        public void EnableExtend()
+        {
+            System.Diagnostics.Process.Start(new ProcessStartInfo
+            {
+                FileName = "DisplaySwitch.exe",
+                Arguments = "/extend",
+                UseShellExecute = true,
+                CreateNoWindow = true
+            });
+        }
+    }
+}

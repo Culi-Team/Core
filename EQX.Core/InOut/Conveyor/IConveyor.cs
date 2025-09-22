2b02fd72b857a3a0066f55f8608a76210854b5e7
Hoang Anh <hoanganh372001@gmail.com>
2025-09-22 16:39:04 +0700

    Add Conveyor Device

---
+++ EQX.Core/InOut/Conveyor/IConveyor.cs
@@ -0,0 +1,17 @@
+﻿using EQX.Core.InOut;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EQX.Core.InOut
+{
+    public interface IConveyor
+    {
+        bool IsError { get; }
+        void Run();
+        void Stop();
+        void RunReverse();
+    }
+}
---
+++ EQX.Core/InOut/Conveyor/IConveyorFactory.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EQX.Core.InOut.Conveyor
+{
+    public interface IConveyorFactory
+    {
+        IConveyor Create(IDInput? inError, IDOutput? outRun, IDOutput? outReverseRun, IDOutput? outStop);
+    }
+}

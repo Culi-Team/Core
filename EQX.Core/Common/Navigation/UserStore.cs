5b5fa15fdb9ab195e3474b061a5c15a6e42bf1d9
NGUYEN PHU TRONG <trongong123@gmail.com>
2025-09-29 15:04:41 +0700

    Add User Login View

--- EQX.Core/Common/Navigation/UserStore.cs
+++ EQX.Core/Common/Navigation/UserStore.cs
@@ -1,18 +1,50 @@
-﻿namespace EQX.Core.Common
+﻿using EQX.Core.Common.Navigation;
+
+namespace EQX.Core.Common
 {
     public class UserStore
     {
-        public event Action UserChanged;
-        private EPermission permission = EPermission.Operator;
+        public event Action? UserChanged;
 
-        public EPermission Permission
+        private EPermission _permission = EPermission.Operator;
+        private string _userName = "Operator";
+        private UserAccount _currentUser = new UserAccount
         {
-            get => permission;
-            set
+            UserName = "Operator",
+            Permission = EPermission.Operator
+        };
+
+        public EPermission Permission => _permission;
+
+        public string UserName => _userName;
+
+        public UserAccount CurrentUser => _currentUser.CloneWithoutPassword();
+
+        public bool IsLoggedIn => _permission != EPermission.Operator || !string.Equals(_userName, "Operator", StringComparison.OrdinalIgnoreCase);
+
+        public bool IsSuperUser => _permission == EPermission.SuperUser;
+
+        public void SetUser(UserAccount account)
+        {
+            if (account == null)
             {
-                permission = value;
-                UserChanged?.Invoke();
+                throw new ArgumentNullException(nameof(account));
             }
+
+            _permission = account.Permission;
+            _userName = account.UserName;
+            _currentUser = account.CloneWithoutPassword();
+
+            UserChanged?.Invoke();
+        }
+
+        public void Reset()
+        {
+            SetUser(new UserAccount
+            {
+                UserName = "Operator",
+                Permission = EPermission.Operator
+            });
         }
     }
 

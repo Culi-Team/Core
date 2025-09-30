919740734e5f12889bb9247798a74c3ec346a66e
Xuanhv21 <xuanhoangvinh01@gmail.com>
2025-09-30 11:56:38 +0700

    Update UserStore

--- EQX.Core/Common/Navigation/UserStore.cs
+++ EQX.Core/Common/Navigation/UserStore.cs
@@ -4,47 +4,17 @@ namespace EQX.Core.Common
 {
     public class UserStore
     {
-        public event Action? UserChanged;
+        public event Action UserChanged;
+        private EPermission permission = EPermission.Operator;
 
-        private EPermission _permission = EPermission.Operator;
-        private string _userName = "Operator";
-        private UserAccount _currentUser = new UserAccount
+        public EPermission Permission
         {
-            UserName = "Operator",
-            Permission = EPermission.Operator
-        };
-
-        public EPermission Permission => _permission;
-
-        public string UserName => _userName;
-
-        public UserAccount CurrentUser => _currentUser.CloneWithoutPassword();
-
-        public bool IsLoggedIn => _permission != EPermission.Operator || !string.Equals(_userName, "Operator", StringComparison.OrdinalIgnoreCase);
-
-        public bool IsSuperUser => _permission == EPermission.SuperUser;
-
-        public void SetUser(UserAccount account)
-        {
-            if (account == null)
+            get => permission;
+            set
             {
-                throw new ArgumentNullException(nameof(account));
+                permission = value;
+                UserChanged?.Invoke();
             }
-
-            _permission = account.Permission;
-            _userName = account.UserName;
-            _currentUser = account.CloneWithoutPassword();
-
-            UserChanged?.Invoke();
-        }
-
-        public void Reset()
-        {
-            SetUser(new UserAccount
-            {
-                UserName = "Operator",
-                Permission = EPermission.Operator
-            });
         }
     }
 

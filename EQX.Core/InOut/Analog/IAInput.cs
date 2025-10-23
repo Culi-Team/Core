bdd1936d58e81c53f9b77419a4d69c22b7763387
IPC-510 <IPC-510@DESKTOP-KKAO543>
2025-10-23 21:37:51 +0700

    Add Analog Device Parameter

--- EQX.Core/InOut/Analog/IAInput.cs
+++ EQX.Core/InOut/Analog/IAInput.cs
@@ -2,8 +2,22 @@
 
 namespace EQX.Core.InOut
 {
+    public interface IAInputParameter
+    {
+        /// <summary>
+        /// Voltage range min value
+        /// </summary>
+        double MinValue { get; }
+        /// <summary>
+        /// Voltage range max value
+        /// </summary>
+        double MaxValue { get; }
+    }
+
     public interface IAInput : IIdentifier
     {
+        IAInputParameter Parameter { get; }
+
         double Volt { get; }
         double Current { get; }
     }
